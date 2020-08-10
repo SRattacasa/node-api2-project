3c385573b13f2076f86aed7cf467acfd76162015
SRattacasa <38533+SRattacasa@users.noreply.github.com>
2020-08-09 20:29:59 -0400

    MVP

--- comments/comments-router.js
+++ comments/comments-router.js
@@ -13,15 +13,30 @@ router.get("/api/posts/:id/comments", (req, res) => {
     })
 })
 
+// router.post("/api/posts/:id/comments", (req, res) => {
+//     db.findById(req.params.id)
+//     .then(comments => {
+//         if (!req.body.text) {
+//             return res.status(400).json({
+//                 message: "Please add text to your comment",
+//             })
+//         }
+//         db.insertComment(req.body.text)
+//         res.status(200).json(req.body.text)
+//     })
+//     .catch(error => { 
+//         res.status(500).json({message: "Aw snap, something ain't right."})
+//     })
+// })
 router.post("/api/posts/:id/comments", (req, res) => {
     if (!req.body.text) {
-		return res.status(400).json({
-			message: "Please add text to your comment",
-		})
-	}
-    db.insertComment(req.params.text)
-    .then(comments => {
-        res.status(200).json(comments)
+        return res.status(400).json({
+            message: "Please add text to your comment",
+        })
+    }
+    db.insertComment({post_id: req.params.id, text: req.body.text})
+    .then(comments => { 
+        res.status(200).json(req.body.text)
     })
     .catch(error => { 
         res.status(500).json({message: "Aw snap, something ain't right."})
